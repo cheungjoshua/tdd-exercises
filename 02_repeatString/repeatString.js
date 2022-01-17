499fc464ab263e715e340e7c8b8f7a9d9afbbae2
Joshua Cheung <contact.cjoshua@gmail.com>
2022-01-16 21:14:20 -0800

    initial commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) 
+{
+    let line = '';
+    if(num < 0) 
+    {
+        return "ERROR";
+    } 
+    else if(num == 0 && string == '') 
+    {
+        return line;
+    } 
+    else 
+    {
+        for(let i = 0; i < num; i++) {
+            line += string;
+        }
+    }
+    return line;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
