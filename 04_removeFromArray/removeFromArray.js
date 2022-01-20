944a4165b31d0e6fc0dc03280945576760de1f07
Joshua Cheung <contact.cjoshua@gmail.com>
2022-01-20 14:48:38 -0800

    finished the remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let list = args[0];
 
+    
+    let myArray = list.filter(item => !args.includes(item));
+    return myArray
 };
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray; 
