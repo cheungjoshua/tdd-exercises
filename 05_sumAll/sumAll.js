71234af638d1dcd52b64bc6194004e4ed9c5f508
Joshua Cheung <contact.cjoshua@gmail.com>
2022-01-21 12:21:17 -0800

    sumAll pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(first_num, sec_num) {
+    if(!Number.isInteger(first_num) || !Number.isInteger(sec_num)){
+        return "ERROR";
+    }
 
+    if(first_num < 0 || sec_num < 0){
+        return "ERROR";
+    }
+    
+    if(first_num > sec_num){
+        end = first_num;
+        start = sec_num;       
+    } 
+    if(first_num < sec_num){
+        start = first_num;
+        end = sec_num;
+    }
+    
+
+    let sum = 0;
+    for(let i = start; i <= end; i ++){
+        sum = sum + i;
+    }
+        
+    return sum;
 };
 
 // Do not edit below this line
